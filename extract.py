74b276b0d0879c9b14df24aa056dfd75b2d09764
Lev Maximov <lev.maximov@gmail.com>
2010-11-22 00:50:47 +0600

    added 7z support

--- extract.py
+++ extract.py
@@ -1,19 +1,19 @@
-import sys,re,os
+import sys, os
 
 def untgz(a):
-    base='.'.join(a)
+    base = '.'.join(a)
     if not os.access(base,os.F_OK):
         os.mkdir(base)
     os.system('bsdtar xzvf "%s" -C "%s"' % (name, base))
 
 def untbz2(a):
-    base='.'.join(a)
+    base = '.'.join(a)
     if not os.access(base,os.F_OK):
         os.mkdir(base)
     os.system('bsdtar xjvf "%s" -C "%s"' % (name, base))
 
 def check(a,ext):
-    if a[len(a)-1]==ext:
+    if a[-1] == ext:
         a.pop()
         return True
     else:
@@ -22,29 +22,31 @@ def check(a,ext):
 #import pdb
 #pdb.set_trace()
 name = sys.argv[1]
-a=re.split('\.', name)
+a = name.split('.')
 #print name
 #a=re.search('(.*)\.([^.]*)',name)
-n=len(a)
-if n>1:
+n = len(a)
+if n > 1:
 #    (base,ext)=a.groups()
-    if check(a,'rar'): 
+    if check(a, 'rar'): 
         os.system('rar x -ad "%s"' % name)
-    elif check(a,'zip'):
+    elif check(a, 'zip'):
         base='.'.join(a)
-        if not os.access(base,os.F_OK):
+        if not os.access(base, os.F_OK):
             os.mkdir(base)
         os.chdir(base)
         os.system('pkzip25 -ext -dir -nozip "../%s"' % name)
     elif check(a, 'tgz'):
         untgz(a)
-    elif check(a,'gz'):
-        if check(a,'tar'):
+    elif check(a, 'gz'):
+        if check(a, 'tar'):
             untgz(a)
         else:
             os.system('gunzip %s' % name)
-    elif check(a,'tbz2') or check(a,'bz2') and check(a,'tar'):
+    elif check(a, 'tbz2') or check(a, 'bz2') and check(a, 'tar'):
         untbz2(a)
+    elif check(a, '7z'):
+        os.system('7z x -o* "%s"' % name)
     else:
         exit('unknown extension')
 else:
